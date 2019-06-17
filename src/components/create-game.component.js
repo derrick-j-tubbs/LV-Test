9887c6a87f2ce6d1cd5dfe99ac352957f9c62c0c
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-06-17 12:36:40 -0400

    Fixed Error with Class name - class was previously CreateTodo as this was a reimplementation of a previous todo list project

--- src/components/create-game.component.js
+++ src/components/create-game.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-export default class CreateTodo extends Component {
+export default class CreateGame extends Component {
 
     constructor(props) {
         super(props);
